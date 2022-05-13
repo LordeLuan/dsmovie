d78c169d75c7e9c7bf16011ff398056816aac694
Luan Lima <lordellu@hotmail.com>
2022-05-13 16:41:33 -0300

    Find movies

---
+++ backend/dsmovie/src/main/java/com/devsuperior/dsmovie/services/MovieService.java
@@ -0,0 +1,38 @@
+package com.devsuperior.dsmovie.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.devsuperior.dsmovie.dto.MovieDTO;
+import com.devsuperior.dsmovie.entities.Movie;
+import com.devsuperior.dsmovie.repository.MovieRepository;
+
+@Service
+public class MovieService {
+
+	@Autowired
+	private MovieRepository repository;
+	
+	@Transactional(readOnly = true)
+	public Page<MovieDTO> findAll(Pageable pageable) {
+//		Listando com paginação, buscando apenas uma quatidade para mostrar na página (quando clicado para ir para próxima pagina de filmes 
+//		será carregada mais informações)
+		Page<Movie> result = repository.findAll(pageable);
+//		Map para percorrer o array e expressão lambda para instanciar dto passando os obj entity
+		Page<MovieDTO> page = result.map(x -> new MovieDTO(x));
+		return page;
+	}
+	
+	@Transactional(readOnly = true)
+	public MovieDTO findById(Long id) {
+		Movie result = repository.findById(id).get();
+		MovieDTO dto = new MovieDTO(result);
+		return dto;
+	}
+	
+	
+	
+}
